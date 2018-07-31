07139a11f964fecc414c278a642383f4d75b9404
htw229 <jenny.giboney@gmail.com>
2018-07-30 23:23:02 -0700

    test

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import BritpickDialects
+admin.site.register(BritpickDialects)
+
+from .models import BritpickFindReplace
+admin.site.register(BritpickFindReplace)
--- app/models.py
+++ app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+class BritpickDialects(models.Model):
+    name = models.CharField(max_length=100, db_index=True)
+
+class BritpickFindReplace(models.Model):
+    dialect = models.ForeignKey(BritpickDialects, blank=True, null=True, db_index=True, on_delete="cascade")
+    searchwords = models.TextField(blank=True, null=True)
+    directreplacement = models.CharField(blank=True, null=True, max_length=200)
+    considerreplacement = models.TextField(blank=True, null=True)
+    clarifyreplacement = models.TextField(blank=True, null=True)
+    active = models.BooleanField(default=True)
+
+
+
+
