b101be7e6354f76b8c2eced5a6245bee0be1e683
htw229 <jenny.giboney@gmail.com>
2018-09-06 02:20:27 -0700

    changed topic admin for friendlier editing

--- app/utils.py
+++ app/utils.py
@@ -1,4 +1,6 @@
-from .models import BritpickDialects, BritpickFindReplace
+from urllib.parse import unquote
+
+from .models import BritpickDialects, BritpickFindReplace, Citation
 from .debug import Debug as DebugClass
 
 debug = DebugClass()
@@ -10,3 +12,20 @@ def changebritishdialectname():
             obj.save()
             debug.add(['obj', obj], max=10)
     debug.print()
+
+
+def addsbaclcitations():
+    urlstart = "https://separatedbyacommonlanguage.blogspot.com/search/label/"
+    labels = ["food%2Fcooking","idioms","pronunciation","adjectives","epithets","medicine%2Fdisease","spelling","WotY","babies and children","education","grammar","fashion%2Fclothing","adverbs","morphology","prepositions","body parts","books","taboo","time","politeness","names","rituals","transport%28ation%29","sport","shopping","games","more complicated than you might think","architecture","holidays","prepositional%2Fphrasal verbs","dialect","sex","politics%2Fhistory","money","AusE","class","interjections","prescriptivism","Canadian count","animals","bodily functions","geography","humo%28u%29r","law","occupations","SAfE","Scrabble","bureaucracy","count%2Fmass","metaphor","numbers","recreation","television","trade names","ScottishE","intoxicants","journalism","music","punctuation","stereotypes","untranslatable","French","clipping","foreign words","measurement","signage","Janus words","Latin","crime%2Fpunishment","furniture","gender","Americanization","U and non-U","communication","euphemism","hardware","housework","hygiene","project ideas","rhoticity","IrishE","auxiliary verbs","cliche","competition","containers","determiners","dictionaries","disability","emotions%2Fmoods","film","negation","plants","plurals","pronouns%2Fproforms","AVIC","conjunctions","guest bloggers","race%2Fethnicity","CanE","Greek","announcements","backformation","blends","colo%28u%29rs","exclamations","overstatement","understatement","Lynneukah","Sweden","alphabet","contractions","death","information structure","office supplies","onomatopoeia","questions","weather","Britishization","German","Italian","NZE","Spanish","computers","global English","nominali%7Bs%2Fz%7Dation","packaging","religion","swedish","symbols","theat%7Ber%2Fre%7D","Dutch","linguistic relativity","puzzle","subjunctive","supernatural","weapons"]
+
+    for labelurl in labels:
+        labeltext = unquote(labelurl)
+
+        url = urlstart + labelurl
+        adminname = "zsbacl-" + labeltext
+        name = "Separated by a Common Language: %s tag" % labeltext
+
+        citation = Citation(name=name, adminname=adminname, url=url)
+        citation.save()
+
+        print(citation)
