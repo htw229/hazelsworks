ddda9bf61b360de139945140504e855f2a4af2a7
htw229 <jenny.giboney@gmail.com>
2018-11-10 22:29:31 -0800

    britpick admin tweaks, model tweaks

--- britpick_app/admin.py
+++ britpick_app/admin.py
@@ -1,55 +1,103 @@
 from django.contrib import admin
 from django import forms
 
-from britpick_app.models import Suggestion, Dialect, Category, Explanation, Reference, Topic, Britpick, Search, SearchString, SearchGroup, SearchVariables, Word, WordGroup, Example, Spelling
+from britpick_app.models import *
 
 
 class BaseAdmin(admin.ModelAdmin):
     exclude = ('date_created', 'date_edited',)
+    save_on_top = True
 
-class DialectAdmin(admin.ModelAdmin):
+
+
+
+class DialectAdmin(BaseAdmin):
     list_display = ('name', 'default', 'description')
 
+admin.site.register(Dialect, DialectAdmin)
 
-class SearchForm(forms.ModelForm):
-    class Meta:
-        model = Search
-        fields = '__all__'
+class TopicAdmin(BaseAdmin):
+    search_fields = ('name',)
 
-    def clean(self):
-        search_string = self.cleaned_data.get('search_string')
-        search_group = self.cleaned_data.get('search_group')
+admin.site.register(Topic, TopicAdmin)
 
-        if search_string and search_group:
-            raise forms.ValidationError('Cannot have both search string and search group')
-        elif not search_string and not search_group:
-            raise forms.ValidationError('Must have either search string or search group')
+class ReferenceAdmin(BaseAdmin):
+    search_fields = ('name',)
 
-        return self.cleaned_data
+admin.site.register(Reference, ReferenceAdmin)
 
-class SearchAdmin(admin.ModelAdmin):
-    form = SearchForm
+class ExplanationAdmin(BaseAdmin):
+    search_fields = ('name',)
 
-admin.site.register(Search, SearchAdmin)
+admin.site.register(Explanation, ExplanationAdmin)
+
+class WordAdmin(BaseAdmin):
+    search_fields = ('name',)
+
+admin.site.register(Word, WordAdmin)
+
+class WordGroupAdmin(BaseAdmin):
+    search_fields = ('name',)
+
+admin.site.register(WordGroup, WordGroupAdmin)
+
+class SearchGroupAdmin(BaseAdmin):
+    search_fields = ('name',)
+
+admin.site.register(SearchGroup, SearchGroupAdmin)
+
+class SearchStringAdmin(BaseAdmin):
+    pass
+
+admin.site.register(SearchString, SearchStringAdmin)
+
+class SearchStringInline(admin.TabularInline):
+    model = SearchString.britpicks.through
+    # model = Britpick.search_strings.through
+    # model = Britpick.search_strings
+    # model = SearchString.britpicks_searched_by.through
+    extra = 0
+    # fields = ('search_strings_string',)
+
+class BritpickAdmin(BaseAdmin):
+
+    change_form_template = 'admin/custom/britpick.html'
+
+    list_display = ('name', 'dialect', 'category', 'active', 'verified', 'date_edited', 'date_created',)
+    list_editable = ('active', 'verified', 'dialect', 'category',)
+    list_filter = ('active', 'verified', 'dialect', 'category', 'explanations',)
+    # create a list_filter to get problematic conflicts (instead of duplicates page)
+    search_fields = ('name',)
+    # add custom search https://docs.djangoproject.com/en/2.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin.get_search_results
+    date_hierarchy = 'date_edited'
+    empty_value_display = '- unnamed -'
 
 
+    exclude = ('name',)
+    autocomplete_fields = ('topics', 'references', 'explanations', 'search_groups', 'exclude_nearby_groups', 'require_nearby_groups', 'word_groups',)
+    inlines = (SearchStringInline,)
+    filter_horizontal = ('topics', 'references', 'words',)
 
+    fieldsets = (
+        (None, {
+            'fields': (('dialect', 'category',), ('active', 'verified',), 'search_groups',),
+        }),
+        ('Advanced search options', {
+            'classes': ('collapse',),
+            'fields': ('exclude_nearby_strings', 'exclude_nearby_groups', 'require_nearby_strings', 'require_nearby_groups',),
+        }),
+        (None, {
+            'fields': ('words', 'word_groups', 'details', 'explanations', 'topics', 'references',)
+        }),
+    )
+
+admin.site.register(Britpick, BritpickAdmin)
 
 
 admin.site.register(Suggestion)
-
-admin.site.register(Dialect, DialectAdmin)
 admin.site.register(Category)
-admin.site.register(Explanation)
 
-admin.site.register(Reference)
-admin.site.register(Topic)
-admin.site.register(Britpick)
-
-admin.site.register(SearchString)
-admin.site.register(SearchGroup)
 admin.site.register(SearchVariables)
-admin.site.register(Word)
-admin.site.register(WordGroup)
+
 admin.site.register(Example)
 admin.site.register(Spelling)
--- britpick_app/models.py
+++ britpick_app/models.py
@@ -66,6 +66,8 @@ class Topic(BaseModel):
         (WORD, 'word'),
     )
 
+    # add get_absolute_url() for view on site to be enabled on edit page
+
     # name = models.CharField(max_length=100, unique=True)
     # active = models.BooleanField(default=True)
     # main_topic = models.BooleanField(default=True)
@@ -105,58 +107,85 @@ class Britpick(BaseModel):
     category = models.ForeignKey("Category", models.CASCADE, related_name='britpicks', blank=True, null=True)
     # For category, if none chosen, generate on the fly from replacement if needed for filtering
 
-    searches = models.ManyToManyField("Search", blank=True)
-
-
-    # Word m2m
-    # Explanation m2m
-    # Topic m2m
-    # Reference m2m
-
-    # details text
-
-
-class Search(BaseModel):
-    """
-    tie together search constructors
-    (slightly more complex through field)
-    """
+    # searches = models.ManyToManyField("Search", blank=True, related_name='britpicks')
 
     # SEARCH FOR
-    search_string = models.ForeignKey("SearchString", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
-    search_group = models.ForeignKey("SearchGroup", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
+    search_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks')
+    search_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks')
 
     # CONTEXT
-    exclude_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='excluded_by')
-    exclude_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='excluded_by')
-    require_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='required_by')
-    require_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='required_by')
+    exclude_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks_excluded_by')
+    exclude_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks_excluded_by')
+    require_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks_required_by')
+    require_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks_required_by')
+
+
+    words = models.ManyToManyField("Word", blank=True, related_name='britpicks')
+    word_groups = models.ManyToManyField("WordGroup", blank=True, related_name='britpicks')
+
+    explanations = models.ManyToManyField("Explanation", blank=True, related_name='britpicks')
+    topics = models.ManyToManyField("Topic", blank=True, related_name='britpicks')
+    references = models.ManyToManyField("Reference", blank=True, related_name='britpicks')
+
+    details = models.TextField(blank=True)
+
+
+# class Search(BaseModel):
+#     """
+#     tie together search constructors
+#     (slightly more complex through field)
+#     """
+#
+#     # SEARCH FOR
+#     search_string = models.ForeignKey("SearchString", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
+#     search_group = models.ForeignKey("SearchGroup", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
+#
+#     # CONTEXT
+#     exclude_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='excluded_by')
+#     exclude_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='excluded_by')
+#     require_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='required_by')
+#     require_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='required_by')
 
 
 
 class SearchString(BaseModel):
 
+    # britpick linked to:
+    #   search_words - word (+groups)
+    #   excluded_words (+groups)
+    #   required_words (+groups)
+    #   replacement_words (+groups)
+
+    # word contains:
+    # to create searches for british words, ie in order to link to topic automatically within text, create britpick for them (could potentially be done automatically); could have selector in britpick to identify these
+
+
     # could potentially have single search with multiple britpicks - "grade" - included in long list of words that link to education topic; also included in britpick to change to 'level' or 'mark'; also in britpicks with different dialects
     # having link to single record allows better detection of duplicates/conflicts
     # word like "piss off" - searchstring "piss off" linked to word- piss off, topic- piss,
 
-    # name = models.CharField(max_length=100) # optional
     string = models.CharField(max_length=300)
     # options created from separate searchwords functions/regex wrapper to create pattern; ie preserve all words, preserve case, beginning of sentence, end of phrase, question, noun
     # inline - preserve word (#), markup, noun, dash
 
     # search patterns (verbose)
     # search patterns (trie)
-    pattern = models.CharField(max_length=300)
+    pattern = models.CharField(max_length=300, blank=True)
 
+    def __str__(self):
+        if self.name:
+            return self.name
+        else:
+            return self.string
 
 class SearchGroup(BaseModel):
     """
     can use with things like idiot that could have multiple results based on dialect
     also use with things contexts to include/exclude (driving context, swimming context)
     """
-    # name = models.CharField(max_length=100)
-    # m2m SearchString
+
+    search_strings = models.ManyToManyField("SearchString", blank=True, related_name='search_groups')
+
 
 class SearchVariables(BaseModel):
     # database-side representation of markup
