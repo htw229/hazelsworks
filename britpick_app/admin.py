792b3b6e071b6feab29e9325ae213b9967252813
htw229 <jenny.giboney@gmail.com>
2018-11-21 02:55:05 -0800

    working on basemodel and individual models

--- britpick_app/admin.py
+++ britpick_app/admin.py
@@ -1,142 +1,142 @@
-from django.contrib import admin
-from django import forms
-
-import nested_admin
-
-from britpick_app.models import *
-
-# using https://linevi.ch/en/django-inline-in-fieldset.html
-
-class BaseAdmin(admin.ModelAdmin):
-    exclude = ('date_created', 'date_edited',)
-    save_on_top = True
-
-    # class Media:
-    #     css = {
-    #         'all': ('admin/admin.css',),
-    #     }
-
-
-
-class DialectAdmin(BaseAdmin):
-    list_display = ('name', 'default', 'description')
-
-admin.site.register(Dialect, DialectAdmin)
-
-class TopicAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(Topic, TopicAdmin)
-
-class ReferenceAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(Reference, ReferenceAdmin)
-
-class ExplanationAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(Explanation, ExplanationAdmin)
-
-class WordAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(Word, WordAdmin)
-
-class WordGroupAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(WordGroup, WordGroupAdmin)
-
-class SearchGroupAdmin(BaseAdmin):
-    search_fields = ('name',)
-
-admin.site.register(SearchGroup, SearchGroupAdmin)
-
-class SearchStringAdmin(BaseAdmin):
-    pass
-
-admin.site.register(SearchString, SearchStringAdmin)
-
-class SearchStringInline(admin.TabularInline):
-    model = Britpick.search_strings.through
-    extra = 0
-    insert_after = 'verified'
-
-    # fields = ('searchstringname',)
-    readonly_fields = ('searchstringname',)
-
-    verbose_name = 'search'
-    verbose_name_plural = 'searches'
-
-    def searchstringname(self, obj):
-        return str(obj)
-
-class ExcludeNearbyStringsInline(admin.TabularInline):
-    model = Britpick.exclude_nearby_strings.through
-    extra = 0
-    insert_after = 'exclude_nearby_groups'
-
-class RequireNearbyStringsInline(admin.TabularInline):
-    model = Britpick.require_nearby_strings.through
-    extra = 0
-    insert_after = 'require_nearby_groups'
-
-
-
-# class SearchStringNestedInline(nested_admin.NestedStackedInline):
-#     model = SearchString
+# from django.contrib import admin
+# from django import forms
 #
-# class SearchStringThroughInline(nested_admin.NestedStackedInline):
-#     # model = Britpick.search_strings.through
-#     model = SearchString.britpicks.through
-#     inlines = [SearchStringNestedInline]
-
-
-class BritpickAdmin(BaseAdmin):
-
-
-    list_display = ('name', 'dialect', 'category', 'active', 'verified', 'date_edited', 'date_created',)
-    list_editable = ('active', 'verified', 'dialect', 'category',)
-    list_filter = ('active', 'verified', 'dialect', 'category', 'explanations',)
-    # create a list_filter to get problematic conflicts (instead of duplicates page)
-    search_fields = ('name',)
-    # add britpick_app search https://docs.djangoproject.com/en/2.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin.get_search_results
-    date_hierarchy = 'date_edited'
-    empty_value_display = '- unnamed -'
-
-
-    exclude = ('name',)
-    autocomplete_fields = ('topics', 'references', 'explanations', 'search_groups', 'exclude_nearby_groups', 'require_nearby_groups', 'word_groups', 'exclude_nearby_strings',)
-    inlines = (SearchStringInline, ExcludeNearbyStringsInline, RequireNearbyStringsInline,)
-    filter_horizontal = ('topics', 'references', 'words',)
-
-    # inlines = [SearchStringThroughInline]
-
-    fieldsets = (
-        (None, {
-            'fields': (('dialect', 'category',), ('active', 'verified',), 'search_groups',),
-        }),
-        ('Advanced search options', {
-            'classes': ('collapse',),
-            'fields': ('exclude_nearby_strings', 'exclude_nearby_groups', 'require_nearby_groups',),
-        }),
-        (None, {
-            'fields': ('words', 'word_groups', 'details', 'explanations', 'topics', 'references',)
-        }),
-    )
-
-    # def search_strings(self):
-    #     url = 'hello'
-    #     return mark_safe()
-
-admin.site.register(Britpick, BritpickAdmin)
-
-
-admin.site.register(Suggestion)
-admin.site.register(Category)
-
-admin.site.register(SearchVariables)
-
-admin.site.register(Example)
-admin.site.register(Spelling)
+# import nested_admin
+#
+# from britpick_app.models import *
+#
+# # using https://linevi.ch/en/django-inline-in-fieldset.html
+#
+# class BaseAdmin(admin.ModelAdmin):
+#     exclude = ('date_created', 'date_edited',)
+#     save_on_top = True
+#
+#     # class Media:
+#     #     css = {
+#     #         'all': ('admin/admin.css',),
+#     #     }
+#
+#
+#
+# class DialectAdmin(BaseAdmin):
+#     list_display = ('name', 'default', 'description')
+#
+# admin.site.register(Dialect, DialectAdmin)
+#
+# class TopicAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(Topic, TopicAdmin)
+#
+# class ReferenceAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(Reference, ReferenceAdmin)
+#
+# class ExplanationAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(Explanation, ExplanationAdmin)
+#
+# class WordAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(Word, WordAdmin)
+#
+# class WordGroupAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(WordGroup, WordGroupAdmin)
+#
+# class SearchGroupAdmin(BaseAdmin):
+#     search_fields = ('name',)
+#
+# admin.site.register(SearchGroup, SearchGroupAdmin)
+#
+# class SearchStringAdmin(BaseAdmin):
+#     pass
+#
+# admin.site.register(SearchString, SearchStringAdmin)
+#
+# class SearchStringInline(admin.TabularInline):
+#     model = Britpick.search_strings.through
+#     extra = 0
+#     insert_after = 'verified'
+#
+#     # fields = ('searchstringname',)
+#     readonly_fields = ('searchstringname',)
+#
+#     verbose_name = 'search'
+#     verbose_name_plural = 'searches'
+#
+#     def searchstringname(self, obj):
+#         return str(obj)
+#
+# class ExcludeNearbyStringsInline(admin.TabularInline):
+#     model = Britpick.exclude_nearby_strings.through
+#     extra = 0
+#     insert_after = 'exclude_nearby_groups'
+#
+# class RequireNearbyStringsInline(admin.TabularInline):
+#     model = Britpick.require_nearby_strings.through
+#     extra = 0
+#     insert_after = 'require_nearby_groups'
+#
+#
+#
+# # class SearchStringNestedInline(nested_admin.NestedStackedInline):
+# #     model = SearchString
+# #
+# # class SearchStringThroughInline(nested_admin.NestedStackedInline):
+# #     # model = Britpick.search_strings.through
+# #     model = SearchString.britpicks.through
+# #     inlines = [SearchStringNestedInline]
+#
+#
+# class BritpickAdmin(BaseAdmin):
+#
+#
+#     list_display = ('name', 'dialect', 'category', 'active', 'verified', 'date_edited', 'date_created',)
+#     list_editable = ('active', 'verified', 'dialect', 'category',)
+#     list_filter = ('active', 'verified', 'dialect', 'category', 'explanations',)
+#     # create a list_filter to get problematic conflicts (instead of duplicates page)
+#     search_fields = ('name',)
+#     # add britpick_app search https://docs.djangoproject.com/en/2.0/ref/contrib/admin/#django.contrib.admin.ModelAdmin.get_search_results
+#     date_hierarchy = 'date_edited'
+#     empty_value_display = '- unnamed -'
+#
+#
+#     exclude = ('name',)
+#     autocomplete_fields = ('topics', 'references', 'explanations', 'search_groups', 'exclude_nearby_groups', 'require_nearby_groups', 'word_groups', 'exclude_nearby_strings',)
+#     inlines = (SearchStringInline, ExcludeNearbyStringsInline, RequireNearbyStringsInline,)
+#     filter_horizontal = ('topics', 'references', 'words',)
+#
+#     # inlines = [SearchStringThroughInline]
+#
+#     fieldsets = (
+#         (None, {
+#             'fields': (('dialect', 'category',), ('active', 'verified',), 'search_groups',),
+#         }),
+#         ('Advanced search options', {
+#             'classes': ('collapse',),
+#             'fields': ('exclude_nearby_strings', 'exclude_nearby_groups', 'require_nearby_groups',),
+#         }),
+#         (None, {
+#             'fields': ('words', 'word_groups', 'details', 'explanations', 'topics', 'references',)
+#         }),
+#     )
+#
+#     # def search_strings(self):
+#     #     url = 'hello'
+#     #     return mark_safe()
+#
+# admin.site.register(Britpick, BritpickAdmin)
+#
+#
+# admin.site.register(Suggestion)
+# admin.site.register(Category)
+#
+# admin.site.register(SearchVariables)
+#
+# admin.site.register(Example)
+# admin.site.register(Spelling)
--- britpick_app/models.py
+++ britpick_app/models.py
@@ -3,47 +3,110 @@ from django.urls import reverse
 
 # britpick_app model
 class BaseModel(models.Model):
-    name = models.CharField(max_length=100, blank=True)
-    active = models.BooleanField(default=True)
+    _name = models.CharField(max_length=100, blank=True)
+    _display_name = models.CharField(max_length=100, blank=True)
+    _description = models.TextField(blank=True)
+    _active = models.BooleanField(default=True)
+    _verified = models.BooleanField(default=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_edited = models.DateTimeField(auto_now=True)
 
     class Meta:
         abstract = True
 
+    @property
+    def name(self):
+        """ for back-end display purposes """
+        if self._name:
+            return self._name
+        elif self._display_name:
+            return self._display_name
+        else:
+            return self.name_model_pk
+
+    @property
+    def name_model_pk(self):
+        return type(self).__name__ + ' [%s]' % str(self.pk)
+
+    @property
+    def name_verbose(self):
+        s = self.name_model_pk
+        if self._name:
+            s += ' ' + self._name
+        if self._display_name:
+            s += ' ' + '(%s)' % self._display_name
+        return s
+
+    @property
+    def display_name(self):
+        """ for front-end display purposes"""
+        if self._display_name:
+            return self._display_name
+        else:
+            return self.name
+
+    @property
+    def display_name_verbose(self):
+        return self.display_name + ': ' + self.description
+
+    @property
+    def description(self):
+        return self._description
+
+    @property
+    def active(self):
+        return self._active
+
+    @property
+    def verified(self):
+        return self._verified
+
     def __str__(self):
         return self.name
 
 
-class Suggestion(models.Model):
-    # can use for back-end notes and front-end suggestions
-    text = models.TextField()
-    britpick = models.ForeignKey("Britpick", on_delete=models.CASCADE, related_name='suggestions', blank=True)
-    topic = models.ForeignKey("Topic", on_delete=models.CASCADE, related_name='suggestions', blank=True)
+# class Suggestion(models.Model):
+#     # can use for back-end notes and front-end suggestions
+#     text = models.TextField()
+#     britpick = models.ForeignKey("Britpick", on_delete=models.CASCADE, related_name='suggestions', blank=True)
+#     topic = models.ForeignKey("Topic", on_delete=models.CASCADE, related_name='suggestions', blank=True)
 
 
-# Create your models here.
+
 class Dialect(BaseModel):
     # name = models.CharField(max_length=100)
     # active = models.BooleanField(default=True)
     default = models.BooleanField(default=False)
-    description = models.TextField(blank=True)
+    spoken = models.BooleanField(default=False)
+    # description = models.TextField(blank=True)
 
-
-
-class Category(BaseModel):
+class BritpickType(BaseModel):
     """ mandatory, suggested, uncommon, informal, slang """
-    dialogue = models.BooleanField(default=False)
-    description = models.TextField(blank=True)
-
-
-class Explanation(BaseModel):
-    # name = models.CharField(max_length=100)
+    # name = models.CharField(max_length=100, blank=True)
     # active = models.BooleanField(default=True)
-    display_text = models.TextField(blank=True)
+    # details = models.TextField(blank=True)
 
-    # def __str__(self):
-    #     return self.name
+class BritpickCategory(BaseModel):
+    # formerly 'explanation'
+    # name = models.CharField(max_length=100, blank=True)
+    # active = models.BooleanField(default=True)
+    default_britpick_type = models.ForeignKey(
+        "BritpickType",
+        on_delete=models.CASCADE,
+        blank=True,
+        null=True,
+        related_name='categories',
+        help_text=
+            """
+            Categories may have specific types they lend themselves to
+            (ex same idea/different word -> mandatory)
+            This allows automatic assigning of types to Britpicks if they aren't manually specified
+            """,
+    )
+
+
+
+
 
 
 class Reference(BaseModel):
@@ -56,6 +119,14 @@ class Reference(BaseModel):
     page_name = models.CharField(max_length=300, blank=True)
 
 
+class Quote(BaseModel):
+    # name = models.CharField(max_length=300, blank=True)
+    # active = models.BooleanField(default=True)
+
+    text = models.TextField(blank=True)
+    reference = models.ForeignKey("Reference", on_delete=models.CASCADE, related_name='quotes', null=True, blank=True,)
+
+
 class Topic(BaseModel):
     """
     topic is a way to display a large amount of text, links, etc with large numbers of relationships
@@ -64,6 +135,18 @@ class Topic(BaseModel):
     WORD = 'W'
     TOPIC_TYPES = (
         (WORD, 'word'),
+    )
+
+    dialect = models.ForeignKey(
+        "Dialect",
+        models.CASCADE,
+        related_name='topics',
+        blank=True,
+        null=True,
+        help_text=
+            """
+            If dialect is selected, all words/britpicks with this dialect will be automatically linked
+            """,
     )
 
     # add get_absolute_url() for view on site to be enabled on edit page
@@ -104,90 +187,41 @@ class Britpick(BaseModel):
     verified = models.BooleanField(default=True)
 
     dialect = models.ForeignKey("Dialect", models.CASCADE, related_name='britpicks', default=Dialect.objects.get(default=True).pk)
-    category = models.ForeignKey("Category", models.CASCADE, related_name='britpicks', blank=True, null=True)
-    # For category, if none chosen, generate on the fly from replacement if needed for filtering
+    type = models.ForeignKey("BritpickType", models.CASCADE, related_name='britpicks', blank=True, null=True)
+    categories = models.ManyToManyField("BritpickCategory", blank=True, related_name='britpicks')
 
-
-
-    # searches = models.ManyToManyField("Search", blank=True, related_name='britpicks')
-
-    # SEARCH FOR
-
-    # search (lines) as text
-    #
-
-
-    # search_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks', verbose_name='search string')
-    # search_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks')
-
-    # CONTEXT
-    # exclude_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks_excluded_by')
-    # exclude_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks_excluded_by')
-    # require_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='britpicks_required_by')
-    # require_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks_required_by')
-
+    # search_strings -> from SearchString fk
+    search_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='britpicks')
 
     words = models.ManyToManyField("Word", blank=True, related_name='britpicks')
     word_groups = models.ManyToManyField("WordGroup", blank=True, related_name='britpicks')
 
-    explanations = models.ManyToManyField("Explanation", blank=True, related_name='britpicks')
     topics = models.ManyToManyField("Topic", blank=True, related_name='britpicks')
+    always_show_topic_names = models.BooleanField(
+        default=False,
+        help_text=
+            """
+                if you want topics to provide context
+                ex 'sports', 'cars', 'clothing'
+            """,
+    )
+
+
     references = models.ManyToManyField("Reference", blank=True, related_name='britpicks')
 
     details = models.TextField(blank=True)
 
 
-# class Search(BaseModel):
-#     """
-#     tie together search constructors
-#     (slightly more complex through field)
-#     """
-#
-#     # SEARCH FOR
-#     search_string = models.ForeignKey("SearchString", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
-#     search_group = models.ForeignKey("SearchGroup", on_delete=models.CASCADE, blank=True, null=True, related_name='searched_by')
-#
-#     # CONTEXT
-#     exclude_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='excluded_by')
-#     exclude_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='excluded_by')
-#     require_nearby_strings = models.ManyToManyField("SearchString", blank=True, related_name='required_by')
-#     require_nearby_groups = models.ManyToManyField("SearchGroup", blank=True, related_name='required_by')
-
-
 
 class SearchString(BaseModel):
-    # britpick = models.ForeignKey("Britpick", on_delete=models.CASCADE, related_name='search_string_fks',)
+    OPTIONS = (('1', 'one'),)
 
-    # manytomany is too hard
-    # single field within britpick doesn't allow reusability
-    # if making foreignkey, can only link it to 1 britpick
-        # can edit in place
-        # have to deal with searching/removing them
-        # will allow
-    # making manytomany with faux textfield input on the form
-        # appreciable benefit beyond finding duplicates?
-    #
-
-    # if need to link to multiple, can use searchgroup
-    #
-
-    # britpick linked to:
-    #   search_words - word (+groups)
-    #   excluded_words (+groups)
-    #   required_words (+groups)
-    #   replacement_words (+groups)
-
-    # word contains:
-    # to create searches for british words, ie in order to link to topic automatically within text, create britpick for them (could potentially be done automatically); could have selector in britpick to identify these
-
-
-    # could potentially have single search with multiple britpicks - "grade" - included in long list of words that link to education topic; also included in britpick to change to 'level' or 'mark'; also in britpicks with different dialects
-    # having link to single record allows better detection of duplicates/conflicts
-    # word like "piss off" - searchstring "piss off" linked to word- piss off, topic- piss,
+    britpick = models.ForeignKey("Britpick", on_delete=models.CASCADE, related_name='search_strings',)
 
     string = models.CharField(max_length=300)
     # options created from separate searchwords functions/regex wrapper to create pattern; ie preserve all words, preserve case, beginning of sentence, end of phrase, question, noun
     # inline - preserve word (#), markup, noun, dash
+
 
     # search patterns (verbose)
     # search patterns (trie)
@@ -205,7 +239,7 @@ class SearchGroup(BaseModel):
     also use with things contexts to include/exclude (driving context, swimming context)
     """
 
-    search_strings = models.ManyToManyField("SearchString", blank=True, related_name='search_groups')
+    search_strings = models.ManyToManyField("SearchString", blank=True, related_name='search_groups',)
 
 
 class SearchVariables(BaseModel):
@@ -214,31 +248,55 @@ class SearchVariables(BaseModel):
     pass
 
 class Word(BaseModel):
-    pass
-    # word = models.CharField(max_length=100)
-    # category (fk) optional
-    # example quotes - quote + reference
+    """
+    name = unique ('Grade (noun)')
+    word = not unique ('grade')
+    """
+    word = models.CharField(max_length=200)
+    dialect = models.ForeignKey(
+        "Dialect",
+        models.CASCADE,
+        related_name='words',
+        blank=True,
+        null=True,
+    )
+    britpick_type = models.ForeignKey(
+        "BritpickType",
+        models.CASCADE,
+        related_name='words',
+        blank=True,
+        null=True,
+        help_text=
+            """
+            For selecting 'slang' or 'informal'.
+            If britpick does not have type, then selection here will determine britpick type. Can also be used to only display certain results (ie skip slang/informal results that aren't in dialogue or if not wanted).
+            """,
+    )
 
-    # make unique? or if not unique make word topic to differentiate (if not unique, then may have quote examples that don't work well)
-    # or make unique back-end word such as ("grade (noun)" and "grade (verb)") with displaying both as "grade"
+    quotes = models.ManyToManyField("Quote", blank=True, related_name='words',)
+    word_topic = models.ForeignKey("Topic", on_delete=models.CASCADE, related_name='words', blank=True, null=True,)
+    related_topics = models.ManyToManyField(
+        "Topic",
+        related_name='related_words',
+        blank=True,
+        help_text=
+            """
+                If it's the word that's the main point (ie insults; 'slag') then select topic here.
+                But if it's the britpick that's the main point (ie kitchen; counter -> worktop) then topic should be linked in britpick.
+            """,
+    )
 
-    # word topic (fk)
-    # topics (m2m)
+
 
 class WordGroup(BaseModel):
-    pass
-    # name = models.CharField(max_length=100)
-    # Word m2m
-
-class Example(BaseModel):
-    text = models.TextField(blank=True)
-    # Reference fk
-    # Word m2m
+    words = models.ManyToManyField("Word", blank=True, related_name='groups',)
 
 
-class Spelling(models.Model):
-    find_string = models.CharField(max_length=100)
-    replace_string = models.CharField(max_length=100)
+
+
+# class Spelling(models.Model):
+#     find_string = models.CharField(max_length=100)
+#     replace_string = models.CharField(max_length=100)
 
 
 
